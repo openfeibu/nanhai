b5d8b717f9faf20090c51634e35a647aa63e6a8f
wzj <wzj@e7ddacb4-e8fb-754f-bc96-44d4153a284f>
2021-11-29 17:06:51 +0000

    git-svn-id: https://39.108.144.9:8443/svn/nanhai@8 e7ddacb4-e8fb-754f-bc96-44d4153a284f

---
+++ public/themes/pc/views/work_arrange.blade.php
@@ -0,0 +1,16 @@
+@foreach($work_times as $key=> $work_time)
+    <div class="item" @if(!$work_time->is_show) style="display: none;" @endif>
+        @if(isset($work_time['work_arrange']) && $work_time['work_arrange'])
+
+            <div class="time-line"> {{ $work_time['start_time'] }} - {{$work_time['end_time'] }} {{ $work_time['work_arrange']['employee_group_name'] }}组 </div>
+            @if(isset($work_time['work_arrange']['employees']))
+                @foreach($work_time['work_arrange']['employees'] as $key=> $employee)
+                    <div class="name-line"> {{ $employee['name'] }} </div>
+                @endforeach
+            @endif
+        @else
+            <div>{{ $work_time['start_time'] }} - {{$work_time['end_time'] }} 未排班</div>
+
+        @endif
+    </div>
+@endforeach
