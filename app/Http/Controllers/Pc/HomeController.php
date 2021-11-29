24411dea01a82b37366606e78dd143d2be473c74
wzj <wzj@e7ddacb4-e8fb-754f-bc96-44d4153a284f>
2021-11-29 16:26:25 +0000

    git-svn-id: https://39.108.144.9:8443/svn/nanhai@6 e7ddacb4-e8fb-754f-bc96-44d4153a284f

--- app/Http/Controllers/Pc/HomeController.php
+++ app/Http/Controllers/Pc/HomeController.php
@@ -46,7 +46,7 @@ class HomeController extends BaseController
                 $end_time = date('Y-m-d H:i:s',strtotime($end_time.' -1 day'));
                 //var_dump($work_time->start_time,$start_time);exit;
             }
-            echo(date("Y-m-d H:i:s").'-'.$start_time.'-'.$end_time."<br>");
+            //echo(date("Y-m-d H:i:s").'-'.$start_time.'-'.$end_time."<br>");
             if(time()>=strtotime($start_time) && time()<strtotime($end_time))
             {
                 $work_time->is_show = 1;
